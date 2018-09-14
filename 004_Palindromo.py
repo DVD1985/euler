37fb385eb0bfa3a0e460ce3dc48db320ddfc9833
DVD1985 <fernandezcaramesdavid@gmail.com>
2018-09-14 19:58:46 +0200

    ejemplo 4 euler

---
+++ 004_Palindromo.py
@@ -0,0 +1,25 @@
+def euler4(s):	
+	a = 999
+	c = 999
+	while (a > 900):
+		for x in s:
+			if (x % a == 0):
+				return a
+			else:
+				a -= 1
+
+	 	
+def palindromos():
+	a = 998001
+	z = 900000
+	s = [] 
+	while (z < a):
+		p = str(z)
+		if (p == p[::-1]):
+			n = int(p)
+			s.append(n)
+		z = z + 1
+	return s
+
+s = palindromos()
+print (euler4(s)) 
